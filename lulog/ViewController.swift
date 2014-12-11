039b1e1d0efc341d15154259ca67621122487775
武仲 <wuzhong@taobao.com>
2014-12-11 15:03:05 +0800

    Initial Commit

---
+++ lulog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lulog
+//
+//  Created by wuzhong on 14/12/11.
+//  Copyright (c) 2014年 github. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
